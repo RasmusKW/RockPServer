7c69f1cd7caeec17cdd800e0528b4d6d50984868
Rasmus Wedelheim <rasmus_w@hotmail.dk>
2018-10-24 11:58:45 +0200

    Added try/catch Added scanner Added input/output streams Tried to add sockets (kinda working) Added read/write of usernames Added strings to tell the client if the players want to start a game or not

--- src/mainServer.java
+++ src/mainServer.java
@@ -52,10 +52,70 @@ class HandleSession implements Runnable{
 	public void run() {
 		
 		
+try{
 		
+			
+			System.out.println("All clients connected");
+			Scanner qScan = new Scanner(System.in);
+			
+			// Create data input and output streams
+			  DataInputStream inputFromClient1 = new DataInputStream(
+			  socket_player1.getInputStream());
+			  DataOutputStream outputToClient1 = new DataOutputStream(
+			  socket_player1.getOutputStream());
+			
+			 // Create data input and output streams
+			  DataInputStream inputFromClient2 = new DataInputStream(
+			  socket_player2.getInputStream());
+			  DataOutputStream outputToClient2 = new DataOutputStream(
+			  socket_player2.getOutputStream());
+		 
+			  DataInputStream inputFromClient3 = new DataInputStream(
+			socket_player3.getInputStream());
+			DataOutputStream outputToClient3 = new DataOutputStream(
+				socket_player3.getOutputStream());
+			  
+		 //--System.out.println("hello4 im after datainputstreams");
+		 
+		//--------------------------READING INPUTS--------------------------
+		 
+		 //reading yes or no want to start a game
+		 String wantToStartGame = inputFromClient1.readUTF();
+		 System.out.println("Client 1 wants to start game: " + wantToStartGame);
+		 //outputToClient1.writeUTF("You are connected to server");
+		 
+		 String wantToStartGame2 = inputFromClient2.readUTF();
+		 System.out.println("Client 2 wants to start game: " + wantToStartGame2);
+		// outputToClient2.writeUTF("You are connected to server");
+		 
+		 String wantToStartGame3 = inputFromClient3.readUTF();
+		 System.out.println("Client 3 wants to start game: " + wantToStartGame3);
+		 //outputToClient3.writeUTF("You are connected to server");
+		 
+		 
+		 //get username
+		 String username1 = inputFromClient1.readUTF();
+		 System.out.println("Client 1 username: " + username1 );
+		 
+		 String username2 = inputFromClient2.readUTF();
+		 System.out.println("Client 2 username: " + username2 );
 		
+		 String username3 = inputFromClient3.readUTF();
+		 System.out.println("Client 3 username: " + username3 );
+		 
+		 
+	     outputToClient1.writeUTF(username2);
+	     outputToClient1.writeUTF(username3);
+	     outputToClient2.writeUTF(username1);
+	     outputToClient2.writeUTF(username3);
+	     outputToClient3.writeUTF(username1);
+	     outputToClient3.writeUTF(username2);
+
+}catch(IOException ex) {
 		
+	
 	}
 	
 	
+}
 }
