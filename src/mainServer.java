f1b33d9c606770a4192d48c2ada4b4b0f8d13229
Simona <simona.stasaityte@gmail.com>
2018-10-24 15:20:08 +0200

    Reading and writing player tools, added if statemements for winning conditions and result as an output to be sent to client. Boolean for playing again.

--- src/mainServer.java
+++ src/mainServer.java
@@ -37,12 +37,14 @@ public class mainServer {
 			} //main
 }
 
+
 class HandleSession implements Runnable{
 	
 	private Socket socket_player1;
 	private Socket socket_player2;
 	private Socket socket_player3;
-	
+	private static int result;
+	private boolean playAgain;
 	public HandleSession(Socket socket_player1, Socket socket_player2, Socket socket_player3){
 		this.socket_player1 = socket_player1;
 		this.socket_player2 = socket_player2;
@@ -110,6 +112,148 @@ try{
 	     outputToClient2.writeUTF(username3);
 	     outputToClient3.writeUTF(username1);
 	     outputToClient3.writeUTF(username2);
+	     
+	     do {
+			 //get tool
+			 
+		     int player1_tool = inputFromClient1.readInt();
+			 System.out.println(username1 + "'s tool: " + player1_tool);
+			 
+		     int player2_tool = inputFromClient2.readInt();
+			 System.out.println(username2 + "'s tool: " + player2_tool);
+			 
+			 int player3_tool = inputFromClient3.readInt();
+			 System.out.println(username3 + "'s tool: " + player3_tool);
+			 
+		     outputToClient1.writeInt(player2_tool);
+		     outputToClient1.writeInt(player3_tool);
+		     
+		     outputToClient2.writeInt(player1_tool);
+		     outputToClient2.writeInt(player3_tool);
+		
+		     outputToClient3.writeInt(player1_tool);
+		     outputToClient3.writeInt(player2_tool);
+		     
+		     
+		     
+		     if((player1_tool == 0 && player2_tool == 0 && player3_tool == 0) ||
+			 	    	(player1_tool == 0 && player2_tool == 2 && player3_tool == 1) ||
+			 	    	(player1_tool == 0 && player2_tool == 1 && player3_tool == 2) ||
+			 	        (player1_tool == 1 && player2_tool == 1 && player3_tool == 1) ||
+			 	        (player1_tool == 1 && player2_tool == 0 && player3_tool == 2) ||
+			 	        (player1_tool == 1 && player2_tool == 2 && player3_tool == 0) ||
+			 	        (player1_tool == 2 && player2_tool == 2 && player3_tool == 2) ||
+			 	        (player1_tool == 2 && player2_tool == 1 && player3_tool == 0) ||
+			 	        (player1_tool == 2 && player2_tool == 0 && player3_tool == 1) ) {
+			 	    	 result = 0;
+			 	    	outputToClient1.writeInt(result);
+			 	    	outputToClient2.writeInt(result);
+			 	    	outputToClient3.writeInt(result);
+			 	     }
+			 	     
+			 	     //PLAYER 1 WIN
+			 	     if((player1_tool == 0 && player2_tool == 2 && player3_tool == 2) ||
+			 	    	(player1_tool == 1 && player2_tool == 0 && player3_tool == 0) ||
+			 	    	(player1_tool == 2 && player2_tool == 1 && player3_tool == 1) ) {
+			 	    	 result = 1;
+			 	    	outputToClient1.writeInt(result);
+			 	    	outputToClient2.writeInt(result);
+			 	    	outputToClient3.writeInt(result);
+			 	     }
+			 	     
+			 	     //PLAYER 2 WIN
+			 	     if((player1_tool == 0 && player2_tool == 1 && player3_tool == 0) ||
+			 	    	(player1_tool == 1 && player2_tool == 2 && player3_tool == 1) ||
+			 	    	(player1_tool == 2 && player2_tool == 0 && player3_tool == 2) ) {
+			 	    	 result = 2;
+			 	    	outputToClient1.writeInt(result);
+			 	    	outputToClient2.writeInt(result);
+			 	    	outputToClient3.writeInt(result);
+			 	    	
+			 	     }
+			 	     
+			 	     //PLAYER 3 WIN
+			 	     if((player1_tool == 0 && player2_tool == 0 && player3_tool == 1) ||
+			 	 	    (player1_tool == 1 && player2_tool == 1 && player3_tool == 2) ||
+			 	 	    (player1_tool == 2 && player2_tool == 2 && player3_tool == 0) ) {
+			 	 	    	 result = 3;
+			 	 	    	outputToClient1.writeInt(result);
+				 	    	outputToClient2.writeInt(result);
+				 	    	outputToClient3.writeInt(result);
+			 	 	 }
+			 	 	     
+			 	     //PLAYER 2 & PLAYER 3 TIE
+			 	     if((player1_tool == 0 && player2_tool == 1 && player3_tool == 1) ||
+			 	 	 	(player1_tool == 1 && player2_tool == 2 && player3_tool == 2) ||
+			 	 	 	(player1_tool == 2 && player2_tool == 0 && player3_tool == 0) ) {
+			 	 	 	    	 result = 4;
+			 	 	 	    	outputToClient1.writeInt(result);
+					 	    	outputToClient2.writeInt(result);
+					 	    	outputToClient3.writeInt(result);
+			 	 	 	 }
+			 	     
+			 	     //PLAYER 1 & PLAYER 2 TIE
+			 	     if((player1_tool == 0 && player2_tool == 0 && player3_tool == 2) ||
+			 	 	 	(player1_tool == 1 && player2_tool == 1 && player3_tool == 0) ||
+			 	 	 	(player1_tool == 2 && player2_tool == 2 && player3_tool == 1) ) {
+			 	 	 	    	 result = 5;
+			 	 	 	    	outputToClient1.writeInt(result);
+					 	    	outputToClient2.writeInt(result);
+					 	    	outputToClient3.writeInt(result);
+			 	 	 	 }
+			 	     
+			 	     //PLAYER 1 & PLAYER 3 TIE
+			 	     if((player1_tool == 0 && player2_tool == 2 && player3_tool == 0) ||
+			 	 	 	(player1_tool == 1 && player2_tool == 0 && player3_tool == 1) ||
+			 	 	 	(player1_tool == 2 && player2_tool == 1 && player3_tool == 2) ) {
+			 	 	 	    	 result = 6;
+			 	 	 	    	outputToClient1.writeInt(result);
+					 	    	outputToClient2.writeInt(result);
+					 	    	outputToClient3.writeInt(result);
+			 	 	 	 }
+			
+			String playAgain1 = inputFromClient1.readUTF();
+			String playAgain2 = inputFromClient2.readUTF();
+			String playAgain3 = inputFromClient3.readUTF();
+			
+			System.out.println("users output" + playAgain1 + playAgain2 + playAgain3);
+		     
+		    if(playAgain1.equals("y") && playAgain2.equals("y") && playAgain3.equals("y") ) {
+		    	
+		    	playAgain = true;
+		    	 System.out.println("inside if " + playAgain);
+		    }
+		    else if(playAgain1.equals("n") || playAgain2.equals("n") || playAgain3.equals("n")){
+		    	playAgain = false;
+		    }
+		    
+		   
+		   
+		   if(playAgain == true){
+		   outputToClient1.writeUTF("Everybody wants to play again! Let's do it");
+		   outputToClient2.writeUTF("Everybody wants to play again! Let's do it");
+		   outputToClient3.writeUTF("Everybody wants to play again! Let's do it");
+		  
+		   outputToClient1.writeBoolean(playAgain);
+		   outputToClient2.writeBoolean(playAgain);
+		   outputToClient3.writeBoolean(playAgain);
+		   
+		   }
+		   
+		   if(playAgain == false){
+			   outputToClient1.writeUTF("A player selected no to play again, ending game");
+			   outputToClient2.writeUTF("A player selected no to play again, ending game");
+			   outputToClient3.writeUTF("A player selected no to play again, ending game");
+			  
+			   outputToClient1.writeBoolean(playAgain);
+			   outputToClient2.writeBoolean(playAgain);
+			   outputToClient3.writeBoolean(playAgain);
+			   
+			   }
+			   
+			}while(playAgain == true);
+			
+			System.exit(0);
 
 }catch(IOException ex) {
 		
